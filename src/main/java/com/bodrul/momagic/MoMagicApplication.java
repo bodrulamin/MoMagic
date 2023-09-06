289554d6414ffce14c66ffee206c68aa2bd70723
Bodrul Amin <bodrul.amin@mislbd.com>
2023-09-06 10:34:27 +0600

    Initial commit

---
+++ src/main/java/com/bodrul/momagic/MoMagicApplication.java
@@ -0,0 +1,13 @@
+package com.bodrul.momagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoMagicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bodrul/momagic/MoMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodrul.momagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoMagicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
